554ba6295f738292642f13ec71a78a08978d0c0f
Reverse <59625582+codedipper@users.noreply.github.com>
2020-10-01 14:43:36 -0400

    Added milliseconds.

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,8 @@
+let milliseconds = 0;
 let seconds = 0;
 let minutes = 0;
 let hours = 0;
+let displayMilliseconds = 0;
 let displaySeconds = 0;
 let displayMinutes = 0;
 let displayHours = 0;
@@ -9,16 +11,27 @@ let interval = null;
 let status = "stopped";
 
 function stopWatch(){
-    seconds++;
+    milliseconds++;
 
-    if (seconds / 60 === 1){
-            seconds = 0;
-            minutes++;
-    
-        if (minutes / 60 === 1){
-            minutes = 0;
-            hours++;
+    if (milliseconds / 10 === 1){
+        milliseconds = 0;
+        seconds++;
+
+            if (seconds / 60 === 1){
+                seconds = 0;
+                minutes++;
+                
+            if (minutes / 60 === 1){
+                minutes = 0;
+                hours++;
+            }
         }
+    }
+
+    if (milliseconds < 10){
+        displayMilliseconds = milliseconds.toString();
+    } else {
+        displayMilliseconds = milliseconds;
     }
 
     if (seconds < 10){
@@ -40,13 +53,13 @@ function stopWatch(){
     }
 
     let time = document.querySelector("#display")
-    time.innerHTML = `${displayHours}:${displayMinutes}:${displaySeconds}`;
+    time.innerHTML = `${displayHours}:${displayMinutes}:${displaySeconds}:${displayMilliseconds}`;
 }
 
 function startStop(){
     if (status === "stopped"){
 
-        interval = window.setInterval(stopWatch, 1000);
+        interval = window.setInterval(stopWatch, 100);
         document.querySelector("#startStop").innerHTML = "Stop";
         status = "started";
     } else {
@@ -58,10 +71,11 @@ function startStop(){
 
 function reset(){
     window.clearInterval(interval);
-    
+            
+    milliseconds = 0;
     seconds = 0;
     minutes = 0;
     hours = 0;
-    document.querySelector("#display").innerHTML = "00:00:00";
+    document.querySelector("#display").innerHTML = "00:00:00:0";
     document.querySelector("#startStop").innerHTML = "Start";
 }
